15417e7561316e02f60bc527bc64d8f29c04e074
Claeg Neander <111316828+claegneander@users.noreply.github.com>
2022-08-23 16:03:54 -0500

    Update entity_death.java

--- src/main/java/com/kazanjima/simplerepair/events/entity_death.java
+++ src/main/java/com/kazanjima/simplerepair/events/entity_death.java
@@ -12,7 +12,8 @@ public class entity_death implements Listener {
     public void onEvent(EntityDeathEvent event){
         Entity entity = event.getEntity();
         EntityType entityType = entity.getType();
-        //Use the getChance to get the chance of a drop based on what the entity is.
+        double chance = getChance(entityType);
+        
         //Roll for drop, and if success, drop naturally where it died.
     }
 
